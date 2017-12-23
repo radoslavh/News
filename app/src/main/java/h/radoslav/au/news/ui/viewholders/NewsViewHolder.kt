baecbe192ebf85bce3ed1f599096017d166c3251
Radoslav <radoslavhlinka@gmail.com>
2017-12-23 16:05:30 +0100

    image load progress

--- app/src/main/java/h/radoslav/au/news/ui/viewholders/NewsViewHolder.kt
+++ app/src/main/java/h/radoslav/au/news/ui/viewholders/NewsViewHolder.kt
@@ -1,11 +1,14 @@
 package h.radoslav.au.news.ui.viewholders
 
-import android.content.ContentUris
+import android.graphics.drawable.Drawable
 import android.net.Uri
 import android.support.v7.widget.RecyclerView
 import android.view.View
 import com.bumptech.glide.Glide
-import com.bumptech.glide.request.RequestOptions
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.target.Target
 import h.radoslav.au.news.models.Article
 import h.radoslav.au.news.utils.TimeUtil
 import kotlinx.android.synthetic.main.item_layout.view.*
@@ -20,8 +23,19 @@ class NewsViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
         Glide.with(this)
                 .load(Uri.parse(news.urlToImage))
+                .listener(object : RequestListener<Drawable> {
+                    override fun onLoadFailed(e: GlideException?, model: Any?, target: Target<Drawable>?, isFirstResource: Boolean): Boolean {
+                        itemView.progress.visibility = View.GONE
+                        return false
+                    }
+
+                    override fun onResourceReady(resource: Drawable?, model: Any?, target: Target<Drawable>?, dataSource: DataSource?, isFirstResource: Boolean): Boolean {
+                        itemView.progress.visibility = View.GONE
+                        return false
+                    }
+                })
                 .into(itemView.list_item_icon);
-    }
+    }!!
 
 
 }
